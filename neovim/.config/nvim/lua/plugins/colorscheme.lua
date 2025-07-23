16bfadffdf89e797e730300801229cb6bc56694b
Ryan Sullenberger <sullenberger@gmail.com>
2025-07-23 13:46:12 -0400

    feat(nvim): change to ayu_dark theme

--- neovim/.config/nvim/lua/plugins/colorscheme.lua
+++ neovim/.config/nvim/lua/plugins/colorscheme.lua
@@ -7,9 +7,14 @@ return {
     },
   },
   {
+    "Shatur/neovim-ayu",
+    lazy = true,
+  },
+  {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "tokyonight-night",
+      -- colorscheme = "tokyonight-night",
+      colorscheme = "ayu-dark",
     },
   },
 }
