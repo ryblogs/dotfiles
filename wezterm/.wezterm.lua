23386442b268ff015e32149e874e3b1cc4df6f56
Ryan Sullenberger <sullenberger@gmail.com>
2026-01-22 20:14:10 -0500

    feat(wezterm): right click to copy/paste

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,8 +1,27 @@
 local wezterm = require 'wezterm'
+local act = wezterm.action
 local config = wezterm.config_builder()
 
 -- Powerline Symbol for the continuous forward slant (/)
 local SEPARATOR = utf8.char(0xe0bc) 
+
+-- Right Click to Copy/Paste
+config.mouse_bindings = {
+  -- Right click: copy if there's a selection, otherwise paste
+  {
+    event = { Down = { streak = 1, button = "Right" } },
+    mods = "NONE",
+    action = wezterm.action_callback(function(window, pane)
+      local has_selection = window:get_selection_text_for_pane(pane) ~= ""
+      if has_selection then
+        window:perform_action(act.CopyTo("ClipboardAndPrimarySelection"), pane)
+        window:perform_action(act.ClearSelection, pane)
+      else
+        window:perform_action(act.PasteFrom("Clipboard"), pane)
+      end
+    end),
+  },
+}
 
 -- Colors
 local TAHOE_GRAY = '#383838'
